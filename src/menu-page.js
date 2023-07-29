7d66be67991271bc3d16b04cba1340b9ae3f4ff4
vinothrobinson <vinothrobinson03@gmail.com>
2023-07-29 13:47:09 -0400

    Added menu items to the menu page

--- src/menu-page.js
+++ src/menu-page.js
@@ -35,6 +35,37 @@ const switchToMenu = () => {
     tabs.appendChild(contactButton)
     siteInfo.appendChild(tabs)
 
+    const foodOptions =  document.createElement('div');
+    foodOptions.classList.add("food-options")
+    foodOptions.innerHTML = `
+        <div class="food-item">
+            <h2 class="food-name">Pikachu Burger</h2>
+            <img class="food-image" src="https://img.huffingtonpost.com/asset/5b9e35c72100003100c5d420.jpeg?ops=scalefit_720_noupscale">
+            <p class=description>This is a burger designed to look like pikachu, it is accompanied by tortilla chips and a salsa dip</p>
+            <p class="price">Price: $12.00<p>
+        </div>
+        <div class="food-item">
+            <h2 class="food-name">Snorlax Plate</h2>
+            <img class="food-image" src="https://www.moshimoshi-nippon.jp/wp/wp-content/uploads/2019/08/b6bb41e5924867b5fcd56e3bc1aa108e.jpg">
+            <p class=description>This is a lunch meal that comes with rice, meat, and an assortment of veggies</p>
+            <p class="price">Price: $15.00<p>
+        </div>
+        <div class="food-item">
+            <h2 class="food-name">Pokémon Latte</h2>
+            <img class="food-image" src="https://soranews24.com/wp-content/uploads/sites/3/2022/01/pokemon-cafe8.jpg">
+            <p class=description>Order custom lattes with the design of any pokemon you wish!</p>
+            <p class="price">Price: $10.00<p>
+        </div>
+        <div class="food-item">
+            <h2 class="food-name">Eevee's Royal Milk Tea</h2>
+            <img class="food-image" src="https://pbs.twimg.com/media/E2c1kijVEAEyHnQ.jpg">
+            <p class=description>An Eevee themed royal milk tea!</p>
+            <p class="price">Price: $8.00<p>
+        </div>
+    `
+
+    siteInfo.appendChild(foodOptions)
+
     menuPage.appendChild(siteInfo)
 
     const picture = document.createElement('div');
